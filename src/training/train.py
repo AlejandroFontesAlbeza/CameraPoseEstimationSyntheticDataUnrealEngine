b9c8d428ba2af9caba928aa38168ee44ab84f59b
AlejandroFontesAlbeza <alexfontesalbeza@gmail.com>
2026-03-09 16:34:03 +0100

    argparse de train realizado y .bats necesarios para mejorar el flujo tambien realizados

--- src/training/train.py
+++ src/training/train.py
@@ -7,8 +7,35 @@ from src.unet.architecture import CustomDataset, Unet
 from tqdm import tqdm
 import os
 
+import argparse
 
-def main(train_img_path, valid_img_path, train_mask_path, valid_mask_path, num_classes, batch_size, num_epochs, finetuning=False):
+parser = argparse.ArgumentParser(description='Train a UNet model for image segmentation')
+
+parser.add_argument('--num_classes', type=int, default=10, help='Number of classes for segmentation')
+parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate')
+parser.add_argument('--batch_size', type=int, default=2, help='Batch size for training')
+parser.add_argument('--num_epochs', type=int, default=100, help='Number of epochs for training')
+parser.add_argument('--finetuning', action='store_true', help='Whether to perform fine-tuning')
+parser.add_argument('--last_model_path', type=str, default='models/unet_modelV2.pth', help='Path to the pre-trained model for fine-tuning')
+parser.add_argument('--new_model_path', type=str, default='models/unet_modelV3.pth', help='Path to save the fine-tuned/trained model')
+parser.add_argument('--step_lr', action='store_true', help='Whether to use StepLR')
+
+args = parser.parse_args()
+
+def main(train_img_path, valid_img_path, train_mask_path, valid_mask_path,
+        num_classes, lr, batch_size, num_epochs, finetuning=False, last_model_path=None, new_model_path=None, step_lr=True):
+
+    print('Starting training with the following parameters:')
+    print(f'  - Number of classes: {num_classes}')
+    print(f'  - Learning rate: {lr}')
+    print(f'  - Batch size: {batch_size}')
+    print(f'  - Number of epochs: {num_epochs}')
+    print(f'  - Use StepLR: {step_lr}')
+    print(f'  - Finetuning: {finetuning}')
+    if finetuning:
+        print(f'  - Last model path: {last_model_path}')
+        print(f'  - New model path: {new_model_path}')
+
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print('Training on:', device)
@@ -35,12 +62,12 @@ def main(train_img_path, valid_img_path, train_mask_path, valid_mask_path, num_c
         print('Is not finetuning, training from scratch')
         None
     else:
-        print('Finetuning, loading model path')
-        model.load_state_dict(torch.load('models/unet_modelV1.pth', map_location=device))
+        model.load_state_dict(torch.load(last_model_path, map_location=device))
+        print(f'Finetuning, loading model path: {last_model_path}')
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
-    #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.8)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.8)
 
     for epoch in range(num_epochs):
         model.train()
@@ -74,11 +101,14 @@ def main(train_img_path, valid_img_path, train_mask_path, valid_mask_path, num_c
                 val_loss += loss.item()
 
         val_average_loss = val_loss / (index + 1)
-        #scheduler.step()
+        if step_lr == True:
+            scheduler.step()
+        else:
+            print('Not using StepLR, lr is fixed')
         
         print(f'Epoch: {epoch} / {num_epochs}, train_loss: {train_average_loss}, val_loss: {val_average_loss}, lr: {optimizer.param_groups[0]["lr"]}') 
 
-    torch.save(model.state_dict(), 'models/unet_modelV2.pth')
+    torch.save(model.state_dict(), new_model_path)
     print('model saved') 
 
 
@@ -90,6 +120,8 @@ if __name__ == "__main__":
     valid_img_path = os.path.join(base_dir, "data/dataset/valid/images")
     valid_mask_path = os.path.join(base_dir, "data/dataset/valid/masks")
 
-    main(train_img_path=train_img_path, valid_img_path=valid_img_path,
-        train_mask_path=train_mask_path, valid_mask_path=valid_mask_path,
-        num_classes=10, batch_size=2, num_epochs=20, finetuning=True)
+
+    main(train_img_path, valid_img_path,
+        train_mask_path, valid_mask_path,
+        num_classes=args.num_classes, lr=args.lr, batch_size=args.batch_size, num_epochs=args.num_epochs,
+        finetuning=args.finetuning, last_model_path=args.last_model_path, new_model_path=args.new_model_path, step_lr=args.step_lr)
