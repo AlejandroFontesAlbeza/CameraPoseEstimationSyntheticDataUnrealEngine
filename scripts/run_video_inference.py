886a22da0af4c5a86e1c3a23d07e211c8bb9d792
AlejandroFontesAlbeza <alexfontesalbeza@gmail.com>
2026-03-21 22:19:36 +0100

    refactorizo parte del modulo de inference y incluimos de manera opcional la mascara en la visualización

--- scripts/run_video_inference.py
+++ scripts/run_video_inference.py
@@ -1,7 +1,4 @@
-import os
 import cv2
-import time
-from PIL import Image
 import numpy as np
 
 import torch
@@ -10,15 +7,15 @@ from torchvision import transforms
 from unet.unet import Unet
 from config import INFERENCE_VIDEO_PATH, INFERENCE_MODEL_PATH
 from utils.palette import inference_color_palette
+from inference.video_utils import mask_generator
 
-
-def inference_video(video_path, model_path):
+def main():
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
     model = Unet(in_channels=3, num_classes=10).to(device)
-    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.load_state_dict(torch.load(INFERENCE_MODEL_PATH, map_location=device))
     print("Model loaded successfully.")
 
     model.eval()
@@ -28,45 +25,24 @@ def inference_video(video_path, model_path):
         transforms.ToTensor(),
     ])
 
-    cap = cv2.VideoCapture(video_path)
-    width, height = 640, 480
-    if not cap.isOpened():
-        print(f"Error opening video file: {video_path}")
-        return
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
 
-        frame_resized = cv2.resize(frame, (width, height))
-        img_pil = Image.fromarray(cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB))
-        input_tensor = transform(img_pil).unsqueeze(0).to(device)
+    resize = (640,480)
+    show_mask = False
 
-        with torch.no_grad():
-            for _ in range(2):
-                _ = model(input_tensor)
+    gen = mask_generator(INFERENCE_VIDEO_PATH, model,
+                        transform, device, resize=resize,
+                        inference_palette=inference_color_palette)
 
-            torch.cuda.synchronize() if torch.cuda.is_available() else None
-            start = time.time()
-            output = model(input_tensor)
-            torch.cuda.synchronize() if torch.cuda.is_available() else None
-            end = time.time()
-            print(f'Inference time: {end-start}')
-
-        predicted_mask = torch.argmax(output, dim=1).squeeze().cpu().numpy()
-        predicted_mask_resized = cv2.resize(predicted_mask.astype(np.uint8), (width, height), interpolation=cv2.INTER_NEAREST)
-        predicted_mask_color = np.zeros((height, width, 3), dtype=np.uint8)
-
-        for class_index, color in inference_color_palette.items():
-            predicted_mask_color[predicted_mask_resized == class_index] = color
-
-        combined = np.hstack((frame_resized, predicted_mask_color))
-        cv2.imshow("Original + Predicted", combined)
+    for frame, predicted_mask_color in gen:
+        if show_mask:
+            display = np.hstack((frame, predicted_mask_color))
+        else:
+            display = frame
+        cv2.imshow('Inference', display)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-    cap.release()
     cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
-    inference_video(INFERENCE_VIDEO_PATH, INFERENCE_MODEL_PATH)
+    main()
