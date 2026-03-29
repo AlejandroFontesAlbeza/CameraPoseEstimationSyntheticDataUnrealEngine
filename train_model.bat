2888f027c4ec662aea5df544278cfe9d607dba02
AlejandroFontesAlbeza <alexfontesalbeza@gmail.com>
2026-03-30 01:00:37 +0200

    agregado jupyter notebook para explicacion breve del roadmap que he seguido para conseguir el resultado final

--- train_model.bat
+++ train_model.bat
@@ -1,2 +1,3 @@
 @echo off
+echo Setting up training parameters...can take a moment if using GPU for the first time.
 python -m src.training.main %*
--- video_inference.bat
+++ video_inference.bat
@@ -1,2 +1,3 @@
 @echo off
+echo Setting up inference parameters...can take a moment if using GPU for the first time.
 python -m src.inference.main %*
